474e87f781a7f9f7aac57b0ad5f89a0bdf621b3f
Vantuz <Shcherbina444@gmail.com>
2025-04-27 10:16:40 +0300

    Save image path in db

--- controllers/Group.controller.js
+++ controllers/Group.controller.js
@@ -79,9 +79,11 @@ module.exports.getOneGroupWithAllMembers = async(req,res,next) =>{
 
 module.exports.createGroupImage = async(req,res,next) =>{
     try {
-        const {params: {groupId}} = req;
-        console.log(req.file);
-        return res.send({groupId})
+        const {params: {groupId}, file: {filename}} = req;
+        
+        const [row,[updatedGroup]]  = await Group.update({imagePath: filename},{where: {id:groupId},returning:true});
+
+        return res.status(200).send(updatedGroup)
     } catch (error) {
         next(error)
     }
